6387f9ba79c6a9338ec78168b3b2ef4704b9f131
Ouma Nyang <ouma.nyang1@gmail.com>
2026-03-21 00:44:53 +0300

    [FEATURE] Installed Prisma ORM and Neon serverless db

---
+++ src/lib/db.ts
@@ -0,0 +1,27 @@
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  (() => {
+    let connectionString = process.env.DATABASE_URL || '';
+    connectionString = connectionString.replace(/^["']|["']$/g, '');
+    connectionString = connectionString.replace('postgresql://', 'postgres://');
+    
+    if (connectionString) {
+      console.log('Connecting via standard pg Adapter...');
+      const pool = new Pool({ connectionString });
+      // Typecast to bypass recent @types/pg mismatch with PrismaPg adapter
+      const adapter = new PrismaPg(pool as any);
+      return new PrismaClient({ adapter });
+    }
+    
+    throw new Error("DATABASE_URL must be specified.");
+  })();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
